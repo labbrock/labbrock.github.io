5a1e437b9e704e78766dc91d838183df0beecc69
Christian Persson <christian@infobaleen.com>
2018-10-04 20:36:30 +0200

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -5,44 +5,24 @@
       <h1 class="headline"> Labbrock.</h1>
 
       <p>
-        Life Science of the future is here – more important than ever – and Sweden should be pioneer in lab
-        communication in 2020. Labbrock [labb-rock] is the Swedish slang expression for white coat named to give a
-        strong association to Life Science Sweden. The vision of Labbrock is to do our best to make that happen –
-        together with our brave customers – based on a social innovation with costickers.
+        Sweden should be pioneer in lab communication within 2021. The vision of Labbrock is to do our best to make that happen – together with you as a customer – based on a social innovation with costickers. We attach value of keeping up the social dimension. That is why we initially have chosen not to sell the costickers online to give us a unique opportunity to get to known your needs.
       </p>
 
       <p>
-        The use of costickers as a coworking tool in a accelerated, focused and helpful approach while encourage social
-        sustainability is unique – especially within labs and Life Science.
+        We will start the journey in Life Science West. We are open-minded to collaborate with all companies, research teams, university educations and health tech ecosystems that have a courage and willingness to be a part of the new way of coworking and communicate in labs.
       </p>
-      <br>
-
-
-      <v-list light subheader>
-        <v-list-tile
-          v-for="item in items"
-          :key="item"
-        >
-          <v-list-tile-action>
-            <v-icon small color="#bc8f8f">favorite</v-icon>
-          </v-list-tile-action>
-
-          <v-list-tile-content>
-            <v-list-tile-title v-html="item"/>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-
-
-      <br>
-      <h4>
-        Launching soon
-      </h4>
 
       <p>
-        Let’s get coinspired by follow <a class="co" href="https://instagram.com/go.labbrock" target="_blank">@go.labbrock</a>.
-        <br>To learn more feel free to contact <a class="co" href="mailto:hello@labbrock.com?Subject=Hello"
-                                                  target="_top">hello@labbrock.com</a>.
+        Coactions Labbrock can do: encourage social sustainability, accelerate and focus coworking, tool for employer branding and marketing, make work culture stand out, faster inclusion and on-boarding, better involve weak speakers and introvert talents, inspire and attract talents of tomorrow.
+      </p>
+
+      <br>
+
+
+      <p>
+        To learn more please share your contact information at <a class="co" href="mailto:hello@labbrock.com?Subject=Hello"
+                                                                  target="_top">hello@labbrock.com</a>.
+        <br>
         <br>
         <span class="co">#lifesciencecostickers</span>
 
@@ -84,6 +64,10 @@
     font-size: 24px !important;
   }
 
+  p {
+    font-size: 16px;
+  }
+
   .theme--light.v-list {
     background: none;
   }
@@ -103,7 +87,7 @@
     data () {
       return {
         items: [
-          'Encourage social sustainability',
+          'encourage social sustainability',
           'Accelerate and focus your coworking',
           'Tool for employer branding and marketing',
           'Make your work culture stand out',
